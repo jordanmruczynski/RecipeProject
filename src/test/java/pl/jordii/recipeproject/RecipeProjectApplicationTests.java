a81ce51c910676f47faad4b4dbfefa9b3f87baea
jordanmruczynski <joromruc@gmail.com>
2023-01-21 18:20:57 +0100

    Initial commit

---
+++ src/test/java/pl/jordii/recipeproject/RecipeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.jordii.recipeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
