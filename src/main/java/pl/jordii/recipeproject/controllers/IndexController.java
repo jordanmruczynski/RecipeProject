93715ce98277c890d7a41b220cc64b247da67f9b
jordanmruczynski <joromruc@gmail.com>
2023-03-30 17:48:14 +0200

    Added database initialization file Added query method for displayign data in repositories for Category and UnitOfMeasure Added simple displaying for Category in IndexController

--- src/main/java/pl/jordii/recipeproject/controllers/IndexController.java
+++ src/main/java/pl/jordii/recipeproject/controllers/IndexController.java
@@ -1,13 +1,34 @@
 package pl.jordii.recipeproject.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.jordii.recipeproject.domain.Category;
+import pl.jordii.recipeproject.repositories.CategoryRepository;
+import pl.jordii.recipeproject.repositories.UnitOfMeasureRepository;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
 
+    private final CategoryRepository categoryRepository;
+    private final UnitOfMeasureRepository unitOfMeasureRepository;
+
+    @Autowired
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"", "/", "index"})
     public String getIndexPage() {
+        Optional<Category> category = categoryRepository.findByDescription("Polish");
+
+        System.out.println("=================");
+        System.out.println(category.get().getId());
+        System.out.println("=================");
+
         return "index";
     }
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into category (description) values ('American');
+insert into category (description) values ('Italian');
+insert into category (description) values ('Polish');
+insert into unit_of_measure (description) values ('Teaspon');
+insert into unit_of_measure (description) values ('Cup');
